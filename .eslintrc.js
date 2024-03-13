ae7fe341336bf1d432fbfaaf82532e0d162eb69b
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-03-12 21:06:16 -0300

    first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  overrides: [
+    {
+      files: ['**/*.ts?(x)'],
+      rules: {
+        'react/react-in-jsx-scope': 'off',
+        curly: 'off',
+      },
+    },
+  ],
 };
