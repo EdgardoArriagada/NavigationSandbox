2a882cb425b41d95322758040623537ccfc89dec
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-03-19 20:05:04 -0300

    add setting pop to home

--- src/presentation/screens/settings/SettingsScreen.tsx
+++ src/presentation/screens/settings/SettingsScreen.tsx
@@ -1,10 +1,20 @@
 import {FC} from 'react';
 import {Text, View} from 'react-native';
+import {gs} from '../../theme';
+import {PrimaryButton} from '../../components/shared/PrimaryButton';
+import {useAppNavigation} from '../../hooks/shared/useAppNavigation';
+import {StackActions} from '@react-navigation/native';
 
 export const SettingsScreen: FC = () => {
+  const {goBack, dispatch} = useAppNavigation();
+
   return (
-    <View>
-      <Text>SettingsScreen</Text>
+    <View style={gs.container}>
+      <Text>Settings Screen</Text>
+      <PrimaryButton onPress={goBack}>Regresar</PrimaryButton>
+      <PrimaryButton onPress={() => dispatch(StackActions.popToTop())}>
+        Go Home
+      </PrimaryButton>
     </View>
   );
 };
