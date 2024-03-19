9b4feed9125d81644cbf5d8478581afbb66ca6f8
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-03-19 19:59:05 -0300

    add product screen

--- src/presentation/hooks/shared/useAppNavigation.ts
+++ src/presentation/hooks/shared/useAppNavigation.ts
@@ -1,5 +1,15 @@
-import {NavigationProp, useNavigation} from '@react-navigation/native';
+import {
+  NavigationProp,
+  RouteProp,
+  useNavigation,
+  useRoute,
+} from '@react-navigation/native';
 import {RootStackParamList} from '../../routes/StackNavigator';
 
 export const useAppNavigation = (): NavigationProp<RootStackParamList> =>
   useNavigation<NavigationProp<RootStackParamList>>();
+
+export const useAppRoute = <T extends keyof RootStackParamList>(): RouteProp<
+  RootStackParamList,
+  T
+> => useRoute<RouteProp<RootStackParamList, T>>();
--- src/presentation/screens/products/ProductScreen.tsx
+++ src/presentation/screens/products/ProductScreen.tsx
@@ -1,10 +1,23 @@
+import {useRoute} from '@react-navigation/native';
 import {FC} from 'react';
 import {Text, View} from 'react-native';
+import {useAppRoute} from '../../hooks/shared/useAppNavigation';
+import {gs} from '../../theme';
 
 export const ProductScreen: FC = () => {
+  const {params} = useAppRoute<'Product'>();
+
   return (
-    <View>
-      <Text>ProductScreen</Text>
+    <View style={gs.container}>
+      <Text>Product Screen</Text>
+      <Text
+        style={{
+          fontSize: 20,
+          textAlign: 'center',
+          marginTop: 20,
+        }}>
+        {params.id} - {params.name}
+      </Text>
     </View>
   );
 };
