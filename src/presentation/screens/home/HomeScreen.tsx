cf96688e2fd82efce0b5dbe878bb0ee8e47c56ed
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-03-19 20:44:14 -0300

    add an aditional drawer

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,11 +1,29 @@
-import {FC} from 'react';
-import {StyleSheet, View} from 'react-native';
+import {FC, useEffect} from 'react';
+import {Pressable, StyleSheet, Text, View} from 'react-native';
 import {gs} from '../../theme';
 import {PrimaryButton} from '../../components/shared/PrimaryButton';
 import {useAppNavigation} from '../../hooks/shared/useAppNavigation';
+import {DrawerActions} from '@react-navigation/native';
+
+const HeaderLeft: FC = () => {
+  const {dispatch} = useAppNavigation();
+
+  return (
+    <Pressable onPress={() => dispatch(DrawerActions.openDrawer())}>
+      <Text>Menu</Text>
+    </Pressable>
+  );
+};
 
 export const HomeScreen: FC = () => {
-  const {navigate} = useAppNavigation();
+  const {navigate, setOptions} = useAppNavigation();
+
+  useEffect(() => {
+    setOptions({
+      headerLeft: HeaderLeft,
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <View
