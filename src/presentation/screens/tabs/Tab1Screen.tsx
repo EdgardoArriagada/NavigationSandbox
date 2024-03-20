ebdfcb7c15abf48efc54a20d2346d6a162c3f97d
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-03-20 20:31:38 -0300

    add icon to tab 1 to check if it went good

--- src/presentation/screens/tabs/Tab1Screen.tsx
+++ src/presentation/screens/tabs/Tab1Screen.tsx
@@ -1,10 +1,11 @@
 import {FC} from 'react';
-import {Text, View} from 'react-native';
+import {View} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export const Tab1Screen: FC = () => {
   return (
     <View>
-      <Text>Tab1Screen</Text>
+      <Icon name="rocket-outline" size={30} color="#900" />
     </View>
   );
 };
