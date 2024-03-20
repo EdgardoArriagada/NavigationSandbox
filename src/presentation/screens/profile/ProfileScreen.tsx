2de3af599581bf9320f4f1fe7a3333c781a51272
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2024-03-19 21:04:00 -0300

    customize profile screen

--- src/presentation/screens/profile/ProfileScreen.tsx
+++ src/presentation/screens/profile/ProfileScreen.tsx
@@ -1,10 +1,26 @@
 import {FC} from 'react';
 import {Text, View} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {PrimaryButton} from '../../components/shared/PrimaryButton';
+import {useAppNavigation} from '../../hooks/shared/useAppNavigation';
+import {DrawerActions} from '@react-navigation/native';
 
 export const ProfileScreen: FC = () => {
+  const {top} = useSafeAreaInsets();
+  const {dispatch} = useAppNavigation();
+
   return (
-    <View>
+    <View
+      style={{
+        flex: 1,
+        paddingHorizontal: 20,
+        marginTop: top + 20,
+      }}>
       <Text>ProfileScreen</Text>
+
+      <PrimaryButton onPress={() => dispatch(DrawerActions.openDrawer())}>
+        Abrir Menu
+      </PrimaryButton>
     </View>
   );
 };
